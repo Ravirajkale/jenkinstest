865901c3997df426006bbe860c607cbe7aa87d91
raviraj <ravirajkale33@gmail.com>
2024-12-17 19:05:16 +0530

    updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (request, response) => {
-  response.send('welcome to my express application')
+  response.send('password yaad rakhlo bhai')
 })
 app.listen(4000, '0.0.0.0', () => {
   console.log('server started on port 4000')
